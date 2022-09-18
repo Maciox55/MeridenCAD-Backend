4ea05c856c189dee655b9133d9a08ba9ac21bd6a
Maciej Bregisz <maciox55@gmail.com>
2022-09-18 00:42:18 -0400

    Git init

---
+++ controllers/callcontroller.js
@@ -0,0 +1,89 @@
+const Call = require('../models/call');
+
+
+// @desc Get all calls
+// @route GET /api/v1/calls/all
+// @access Public
+exports.getCalls =  (req, res, next) => {
+    Call.find().sort({end:1},{'_id':0,'__v':0}).then(calls => {
+
+        res.status(200).json({
+            success: true,
+            msg: 'Get all calls',
+            calls
+        });
+    }).catch(err => {
+        res.status(500).json({
+            success: false,
+            msg: 'Server error retrieving calls'
+        });
+    });
+    
+}
+
+// @desc Get active calls
+// @route GET /api/v1/calls/active
+// @access Public
+exports.getActiveCalls =  (req, res, next) => {
+    Call.find({end:null,coordinates:{$ne:null}},{'_id':0,'__v':0}).sort({start:1}).then(calls => {
+
+        res.status(200).json({
+            success: true,
+            msg: 'Get all active calls',
+            calls
+        });
+    }).catch(err => {
+        res.status(500).json({
+            success: false,
+            msg: 'Server error retrieving active calls'
+        });
+    });
+    
+}
+
+// @desc Get closed calls
+// @route GET /api/v1/calls/closed
+// @access Public
+exports.getClosedCalls =  (req, res, next) => {
+    Call.find({end:{$ne: null}},{'_id':0,'__v':0}).sort({start:1}).then(calls => {
+
+        res.status(200).json({
+            success: true,
+            msg: 'Get all closed calls',
+            calls
+        });
+    }).catch(err => {
+        res.status(500).json({
+            success: false,
+            msg: 'Server error retrieving closed calls'
+        });
+    });
+    
+}
+
+// @desc Get calls on date
+// @route GET /api/v1/calls/all/:date
+// @access Public
+exports.getCallsOnDate =  (req, res, next) => {
+
+    var daystart = new Date(req.params.date);
+    var dayend = new Date(daystart.getFullYear(), daystart.getMonth(), daystart.getDate()+1,daystart.getHours(),daystart.getMinutes(),daystart.getSeconds());  
+    console.log(daystart);
+    console.log(dayend);
+    // var startOfToday = new Date(date.getFullYear(), date.getMonth(), date.getDate(),date.getHours(),date.getMinutes(),date.getSeconds());  
+    // console.log(startOfToday);
+    Call.find({start:{$gte: daystart,$lt: dayend}},{'_id':0,'__v':0}).then(calls => {
+
+        res.status(200).json({
+            success: true,
+            msg: 'Get all closed calls',
+            calls
+        });
+    }).catch(err => {
+        res.status(500).json({
+            success: false,
+            msg: 'Server error retrieving active calls'
+        });
+    });
+    
+}
---
+++ index.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const crawler = require('../Crawler/index');
+const express = require("express");
+const cron = require('node-cron');
+const dotenv = require('dotenv');
+const rateLimit = require('express-rate-limit');
+const mongoose = require('mongoose');
+dotenv.config();
+
+const limiter = rateLimit({
+  windowMs: 5 * 60 * 1000, // 15 minutes
+	max: 50, // Limit each IP to 50 requests per `window` (here, per 15 minutes)
+  message: "Too many API requests, please wait 5 minutes and try again later.",
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+const PORT = process.env.PORT || 3001;
+cron.schedule("*/5 * * * *", crawler.crawl);
+const app = express();
+app.use(express.json());
+app.use(express.static(path.resolve(__dirname, '../Frontend-Angular/dist/frontend')));
+
+mongoose.connect(process.env.CONNSTRING);
+console.log("Connected to DB...");
+
+
+
+app.use('/api/v1',limiter);
+
+app.use('/api/v1/calls', require('./routes/calls'));
+
+// All other GET requests not handled before will return our React app
+app.get('/*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../Frontend-Angular/dist/frontend', 'index.html'));
+});
+// Handle GET requests to /api route
+app.get("/api", (req, res) => {
+  res.json({ message: "This is not the endpoint you are looking for... *Hand waves in front of face*" });
+});
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
